e6d3a11c8c3ac824835fdaf90e2ecfe2298f9989
Jalpan Thakkar <jalpan119@gmail.com>
2026-06-23 21:23:55 +0530

    feat: baseline spring boot infrastructure with web and mongodb dependencies

---
+++ src/test/java/com/jalpan/acoustic_sentinel_backend/AcousticSentinelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalpan.acoustic_sentinel_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcousticSentinelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
